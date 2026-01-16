96f04dcbe8c25c48d94f2b6938a0ae84014fcec2
aaruiz <aaruiz@uat.edu.mx>
2026-01-15 19:11:57 -0600

    'Prueba2'

--- components/ui/animated-group.tsx
+++ components/ui/animated-group.tsx
@@ -116,13 +116,14 @@ function AnimatedGroup({
   const itemVariants = variants?.item || selectedVariants.item;
 
   const MotionComponent = React.useMemo(
-    () => motion.create(as as keyof JSX.IntrinsicElements),
+    () => motion(as),
     [as]
-  );
+  ) as React.ComponentType<any>;
+  
   const MotionChild = React.useMemo(
-    () => motion.create(asChild as keyof JSX.IntrinsicElements),
+    () => motion(asChild),
     [asChild]
-  );
+  ) as React.ComponentType<any>;
 
   return (
     <MotionComponent
